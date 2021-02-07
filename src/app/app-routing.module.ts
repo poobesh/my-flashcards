6027cb3ba33943c7d5f8356657e76faf5f3afad5
Appadurai <Poobesh.Appadurai@accolite.com>
2021-02-07 12:52:03 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CardsComponent } from './cards/cards.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: CardsComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
